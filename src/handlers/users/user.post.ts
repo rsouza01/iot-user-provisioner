23cfa5a655905ecdfc04a40547618b2e95725c56
Rodrigo de Souza <rsouza01@gmail.com>
2019-11-23 00:55:46 +0100

    Cucumber template created

--- src/handlers/users/user.post.ts
+++ src/handlers/users/user.post.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
-import * as HttpStatus  from 'http-status-codes';
+import * as HttpStatus from 'http-status-codes';
 
 
 export async function main(req: Request, res: Response) {
-  return res.status(HttpStatus.NOT_IMPLEMENTED).send({});
+  //return res.status(HttpStatus.BAD_REQUEST).send(undefined);
+
+  res.writeHead(400, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify(
+      {
+        message: 'Payload should not be empty',
+      }
+    ));
+  return;
 }
--- test/e2e/steps/user.steps.ts
+++ test/e2e/steps/user.steps.ts
@@ -1,17 +1,21 @@
 import { binding, given, then, when } from 'cucumber-tsflow';
-import { assert } from 'chai';
+import assert from 'assert';
 
 import superagent from 'superagent';
 
 @binding()
 export class UserCreateSteps {
 
+  private response: any;
+  private responsePayload: any;
   private request: any;
-  private result: any;
-  private error: any;
 
   @when(/the client creates a POST request to \/user/)
   public createPost() {
+    const url =  `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/user`;
+    console.log('URL: ' + url);
+    
+    this.request = superagent('POST',url);
     this.request = superagent('POST', 'localhost:8080/user');
   }
 
@@ -23,30 +27,40 @@ export class UserCreateSteps {
   @when(/sends the request/)
   public sendRequest(callback) {
     this.request.then((response) => {
-      this.result = response.res;
+      this.response = response.res;
       callback();
     })
       .catch((errResponse) => {
-        this.error = errResponse.response;
+        this.response = errResponse.response;
         callback();
       });
   }
 
   @then(/our API should respond with a 400 HTTP status code/)
   public checkHTTPResponse() {
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>', this.error.statusCode);
-    
-    if (this.error.statusCode !== 400) {
-      throw new Error();
-    }
+    assert.equal(this.response.statusCode, 400);
   }
 
   @then(/the payload of the response should be a JSON object/)
   public checkPayloadContentType() {
-  }
+    // Check Content-Type header 
+    const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Response not of Content-Type application/json');
+    }
+    // Check it is valid JSON 
+    try {
+      this.responsePayload = JSON.parse(this.response.text);
+    } catch (e) {
+      throw new Error('Response not a valid JSON object');
+    }
+  };
 
   @then(/contains a message property which says "Payload should not be empty"/)
   public checkPayloadMessage() {
+    if (this.responsePayload.message !== 'Payload should not be empty') {
+      throw new Error();
+    }
   }
 
 }
