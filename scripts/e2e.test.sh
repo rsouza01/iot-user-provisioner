04b97f51064f3ff12f22623200a038e06818eb19
Rodrigo de Souza <rsouza01@gmail.com>
2019-12-06 00:23:02 +0100

    Comments added to the shell script file

--- scripts/e2e.test.sh
+++ scripts/e2e.test.sh
@@ -8,8 +8,10 @@ if ss -lnt | grep -q :$SERVER_PORT; then
     exit 1; 
 fi
 
+# Make sure we are creating clean images
 docker rm -f $(docker ps -a -q)
 
+# Set the environment (Database, MQTT, etc)
 docker-compose -f ./scripts/docker-compose.yml up -d
 
 # Wait until mongo logs that it's ready (or timeout after 60s)
@@ -23,18 +25,21 @@ done
 # TEST Mongo: nc -v localhost 27017
 echo "Connected in  ($COUNTER seconds so far)"
 
+# Start the server (production-mode) in background
 npm run start &
 
+# Test if the server is up and running
 RETRY_INTERVAL=0.2 
 until ss -lnt | grep -q :$SERVER_PORT; do 
     sleep $RETRY_INTERVAL 
 done
 
+# Run the End-to-End tests
 npx dotenv -e .env cucumber-js -- ./test/e2e/features/**/*.feature --require-module ts-node/register --require ./test/e2e/steps/**/*.steps.ts
 
-
+# Kill everyting
 SIGTERM signal
 kill -15 0
 
-# Tearing down
+# Tear down
 docker-compose down
