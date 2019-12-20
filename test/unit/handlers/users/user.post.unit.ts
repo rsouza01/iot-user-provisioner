7f639b49c1c28e87f4453d97b9be7f00515f54ca
Rodrigo de Souza <rsouza01@gmail.com>
2019-12-20 11:31:13 +0100

    Tests added for generateValidationErrorMessage

--- test/unit/handlers/users/user.post.unit.ts
+++ test/unit/handlers/users/user.post.unit.ts
@@ -1,22 +1,20 @@
-import { expect } from 'chai';
-import 'mocha';
+import { expect } from "chai";
+import "mocha";
 
-import superagent from 'superagent';
+import superagent from "superagent";
 
 const getValidPayload = () => {
   return {
-    email: 'e@ma.il',
-    password: 'password',
+    email: "e@ma.il",
+    password: "password"
   };
-}
+};
 
 const server = `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/user`;
 
-describe('User - POST Handler', () => {
-
-  it.only('Create user successfully', async () => {
-
-    const request = superagent('POST', server);
+describe("User - POST Handler", () => {
+  it.skip("Create user successfully", async () => {
+    const request = superagent("POST", server);
 
     let response;
 
@@ -24,15 +22,14 @@ describe('User - POST Handler', () => {
 
     await request
       .send(JSON.stringify(payload))
-      .set('Content-Type', 'application/json')
-      .then((res) => {
+      .set("Content-Type", "application/json")
+      .then(res => {
         expect(res).not.to.be.undefined;
         expect(res.text).not.to.be.undefined;
       })
-      .catch((error) => {
+      .catch(error => {
         expect(error).to.be.undefined;
         expect(true).to.be.false;
       });
-
   });
 });
